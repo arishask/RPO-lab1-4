37d2088fa7447880917a6563b23c62df28e1ad1d
arishask <125388482+arishask@users.noreply.github.com>
2023-03-23 01:17:03 +0300

    LAB2

--- app/src/main/java/com/example/laba1/MainActivity.java
+++ app/src/main/java/com/example/laba1/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.laba1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
 import com.example.laba1.databinding.ActivityMainBinding;
-
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -15,35 +22,63 @@ public class MainActivity extends AppCompatActivity {
     public static native byte[] randomBytes(int no);
     public static native byte[] encrypt(byte[] key, byte[] array);
     public static native byte[] decrypt(byte[] key, byte[] array);
+    private ActivityMainBinding binding;
 
-    // Used to load the 'laba1' library on application startup.
     static {
         System.loadLibrary("laba1");
         System.loadLibrary("mbedcrypto");
     }
-
-    private ActivityMainBinding binding;
+    ActivityResultLauncher activityResultLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        activityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Intent data = result.getData();
+                            // Handle the Intent
+                            String pin = data.getStringExtra("pin");
+                            Toast.makeText(MainActivity.this, pin,
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
         int res = initRng();
         byte[] v = randomBytes(10);
-
-
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
-        // Example of a call to a native method
-        TextView tv = binding.sampleText;
-        tv.setText(stringFromJNI());
+////        TextView tv = binding.sampleText;
+////        tv.setText(stringFromJNI());
     }
-    /**
-     * A native method that is implemented by the 'laba1' native library,
-     * which is packaged with this application.
-     */
 
+    public static byte[] stringToHex(String s) {
+        byte[] hex;
+        try {
+            hex = Hex.decodeHex(s.toCharArray());
+        } catch (DecoderException ex) {
+            hex = null;
+        }
+        return hex;
+    }
+
+    public void onButtonClick(View v)
+    {
+//        Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
+//        byte[] key = stringToHex("0123456789ABCDEF0123456789ABCDE0");
+//        byte[] enc = encrypt(key, stringToHex("000000000000000102"));
+//        byte[] dec = decrypt(key, enc);
+//        String s = new String(Hex.encodeHex(dec)).toUpperCase();
+//        Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
+        Intent it = new Intent(this, PinpadActivity.class);
+//        startActivity(it);
+        activityResultLauncher.launch(it);
+    }
 
 }
